9fb72f07b598d6cc154a1da5cafc8736041eaa2b
Gregory R. Everitt <geveritt@drew.edu>
2012-06-14 22:50:09 -0400

    first commit

---
+++ Raycaster/Raycaster.cpp
@@ -0,0 +1,15 @@
+//Raycaster.cpp
+  // compile with: /EHsc /LD
+
+#include "Raycaster.h"
+//#include <stdio.h>
+
+namespace RaycasterDll
+{
+	void Raycaster::castRays() {
+		/*
+		// shameless pseudo-code
+
+		*/
+	}
+}
---
+++ Raycaster/Raycaster.h
@@ -0,0 +1,11 @@
+// Raycaster.h
+
+  namespace RaycasterDll
+  {
+      class Raycaster
+      {
+      public:
+          // right now it just prints some pre-determined nonsense to stdout
+          static __declspec(dllexport) void castRays();
+	  };
+  }
