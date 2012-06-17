a546b1f5b9777ae87fb63fa80b6cf2cf237a5be6
Gregory R. Everitt <geveritt@drew.edu>
2012-06-17 14:53:17 -0400

    Added iterator castRays() for not-yet-implemented function findDistance()

--- Raycaster/Raycaster.cpp
+++ Raycaster/Raycaster.cpp
@@ -1,10 +1,9 @@
 //Raycaster.cpp
   // compile with: /EHsc /LD
 
-#include <math.h>
 #include "Raycaster.h"
 #include <SFML\Graphics.hpp>
-//#include <stdio.h>
+using namespace sf;
 
 //definitions for height and width; will change these to passable parameters later
 #define WIDTH 800
@@ -15,31 +14,41 @@ namespace RaycasterDll
 	//////////////////////////////////////////////////////////////////////
 	// Renders vertical lines of increasing redness and decreasing height
 	//////////////////////////////////////////////////////////////////////
-	void Raycaster::render() {
-	sf::RenderWindow App(sf::VideoMode(WIDTH, HEIGHT, 32), "SFML Window Graphics yo"); // create window with desktop settings
-	// main loop
-	//bool isRunning = true;
-	while (App.IsOpened())
+	void Raycaster::render()
 	{
-		sf::Event Event;
-		while (App.GetEvent(Event))
+		RenderWindow App(VideoMode(WIDTH, HEIGHT, 32), "SFML Window Graphics yo"); // create window with desktop settings
+		// main loop
+		//bool isRunning = true;
+		while (App.IsOpened())
 		{
-			// Window closed
-			if (Event.Type == sf::Event::Closed)
-				App.Close();
+			Event Event;
+			while (App.GetEvent(Event))
+			{
+				// Window closed
+				if (Event.Type == Event::Closed)
+					App.Close();
 
-			// Escape key pressed
-			if ((Event.Type == sf::Event::KeyPressed) && (Event.Key.Code == sf::Key::Escape))
-				App.Close();
-		}
+				// Escape key pressed
+				if ((Event.Type == Event::KeyPressed) && (Event.Key.Code == Key::Escape))
+					App.Close();
+			}
 		
-		App.Clear();
+			App.Clear();
+			for (int i = 0; i < WIDTH; i++)
+			{
+				App.Draw(Shape::Line(i,0,i,HEIGHT-i,1,Color(std::min(255, i), 0, 0))); 
+			}
+			//App.Draw(sf::Shape::Line(50,0,50,HEIGHT,1,sf::Color(100, 0, 0))); 
+			App.Display();
+		}
+	}
+	int* castRays()
+	{
+		static int rays[WIDTH];
 		for (int i = 0; i < WIDTH; i++)
 		{
-			App.Draw(sf::Shape::Line(i,0,i,HEIGHT-i,1,sf::Color(std::min(255, i), 0, 0))); 
+			//rays[i] = findDistance(i);
 		}
-		//App.Draw(sf::Shape::Line(50,0,50,HEIGHT,1,sf::Color(100, 0, 0))); 
-		App.Display();
-	}
+		return rays;
 	}
 }
--- Raycaster/Raycaster.h
+++ Raycaster/Raycaster.h
@@ -5,7 +5,9 @@
       class Raycaster
       {
       public:
-          // right now it just prints some pre-determined nonsense to stdout
+          // right now it just displays some arbitrary graphical junk
           static __declspec(dllexport) void render();
+		  // very much a work in progress
+		  static __declspec(dllexport) int* castRays();
 	  };
   }
