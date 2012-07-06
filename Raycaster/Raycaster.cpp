906059a3abc327e993ae22138f9b075a8fbdc683
Gregory R. Everitt <geveritt@gregserv>
2012-07-06 15:46:04 -0400

    testRender() now shades scene so that center is brightest and brightness decreases the closer one is to the sides.

--- Raycaster/Raycaster.cpp
+++ Raycaster/Raycaster.cpp
@@ -41,9 +41,18 @@ namespace RaycasterDll
 			}
 		
 			App.Clear();
-			for (int i = 0; i < WIDTH; i++)
+			int i = 0;
+			while (i < WIDTH/2)
 			{
-				App.Draw(Shape::Line(i,i,i,HEIGHT-i,1,Color(std::min(255, i), 0, 0))); 
+				App.Draw(Shape::Line(i,i,i,HEIGHT-i,1,Color(std::min(255, i), 0, 0)));
+				i++;
+			}
+			int j = WIDTH/2;
+			while (i < WIDTH)
+			{
+				j--;
+				App.Draw(Shape::Line(i,i,i,HEIGHT-i,1,Color(std::min(255, j), 0, 0)));
+				i++;
 			}
 			//App.Draw(sf::Shape::Line(50,0,50,HEIGHT,1,sf::Color(100, 0, 0))); 
 			App.Display();
